e8621631b2ede144e2ef568a253bde653830cd82
Gustavo Erick <gustavoerickv@gmail.com>
2026-03-14 19:11:54 -0300

    adiciona componentes do `primevue`

--- src/modules/primevue.modules.ts
+++ src/modules/primevue.modules.ts
@@ -3,8 +3,16 @@
 import type { App } from 'vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import DataView from 'primevue/dataview'
+import InputNumber from 'primevue/inputnumber'
+import ConfirmDialog from 'primevue/confirmdialog'
+import Toast from 'primevue/toast'
+
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
 
 export function setPrimeVue(app: App) {
   app.use(PrimeVue, {
@@ -12,7 +20,13 @@ export function setPrimeVue(app: App) {
       preset: Aura,
     },
   })
+  app.use(ConfirmationService)
+  app.use(ToastService)
 
   app.component('Button', Button)
   app.component('Card', Card)
+  app.component('DataView', DataView)
+  app.component('InputNumber', InputNumber)
+  app.component('ConfirmDialog', ConfirmDialog)
+  app.component('Toast', Toast)
 }
